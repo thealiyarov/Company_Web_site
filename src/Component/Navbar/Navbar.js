9d0aacc33cf2fd47fbdbf892bbb20e0e0a247b16
Mushfig <mushfigaliyarov@gmail.com>
2023-12-30 00:48:42 +0400

    line

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -37,6 +37,7 @@ function Navbar() {
             </label>
           </div>
         </div>
+        <div className="navbar-line w-[90%] h-[1px] bg-[#a7a6b7] "></div>
       </div>
     </div>
   );
