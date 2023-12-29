210691e6c85b208f5a27fea63cf06fbf3d5329db
Aytac <aytacmurselzade381@gmail.com>
2023-12-29 23:37:42 +0400

    new

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -8,9 +8,20 @@ function Navbar() {
   return (
     <div className="flex bg-[#24204A]">
       <div className=" flex flex-col  mx-auto h-[72px] justify-center">
-      <div className="navbar-main  text-white   w-[1170px] h-[72px] flex justify-between items-center">
-        <div className="navbar-left">
-          <p>one position</p>
+        <div className="navbar-main  text-white   w-[1170px] h-[72px] flex justify-between items-center">
+          <div className="navbar-left">
+            <p>one position</p>
+          </div>
+          <div className=" navbar-right  ">
+            <ul className="flex gap-[60px] text-[#a7a6b7]">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/products">Products</NavLink></li>
+              <li><NavLink to="/catalogs">Catalogs</NavLink></li>
+              <li className="flex gap-[14px]"><NavLink to="/contact">Contact</NavLink></li>
+              <li><NavLink to="/blog">Blog</NavLink></li>
+            </ul>
+          </div>
+          <div className="navbar-line w-[90%] h-[1px] bg-[#a7a6b7] "></div>
         </div>
         <div className=" navbar-right  ">
           <ul className="flex gap-[60px] text-[#a7a6b7]">
