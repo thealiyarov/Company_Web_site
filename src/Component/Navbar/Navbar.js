7a79776d2ca6b93965c49ae7910d54b731017132
Mushfig <mushfigaliyarov@gmail.com>
2023-12-30 00:14:21 +0400

    about page name updated

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
                   <NavLink to="/contact">Contact</NavLink>
                 </li>
                 <li>
-                  <NavLink to="/about">About</NavLink>
+                  <NavLink to="/about">About Us</NavLink>
                 </li>
               </ul>
             </label>
